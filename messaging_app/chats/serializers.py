f03265df6cf4ed50f9efe6ca4711fca72444f04b
Emmanuel Rotich <71790505+mikemanuu@users.noreply.github.com>
2024-12-18 19:54:02 +0300

    Serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import User, Message, Conversation
 from .serializers import UserSerializer, MessageSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import CharField
+from rest_framework.serializers import SerializerMethodField
 
-# User serializer
+# User Serializer
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,10 +18,17 @@ class UserSerializer(serializers.ModelSerializer):
 # Message Serializer
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    time_since_sent = SerializerMethodField()
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender',
+                  'message_body', 'sent_at', 'time_since_sent']
+
+    def get_time_since_sent(self, obj):
+        from datetime import datetime
+        time_diff = datetime.now() - obj.sent_at
+        return str(time_diff.days) + " days ago"
 
 
 # Conversation Serializer
@@ -29,3 +39,18 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'messages', 'created_at']
+
+
+# Custom Validation to ensure a user cannot be both sender and receiver)
+class MessageCreateSerializer(serializers.ModelSerializer):
+    sender = serializers.UUIDField()
+    receiver = serializers.UUIDField()
+
+    class Meta:
+        model = Message
+        fields = ['sender', 'receiver', 'message_body', 'sent_at']
+
+    def validate(self, data):
+        if data['sender'] == data['receiver']:
+            raise ValidationError("Sender and receiver cannot be the same.")
+        return data
