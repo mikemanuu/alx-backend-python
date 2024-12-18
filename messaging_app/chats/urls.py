00e77e3b0407a4f89d17d52f239c99bf43a71c68
Emmanuel Rotich <71790505+mikemanuu@users.noreply.github.com>
2024-12-18 19:45:38 +0300

    Added Serializers

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import ConversationListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/conversations/', ConversationListView.as_view(),
+         name='conversation_list'),
 ]
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Conversation
+from .serializers import ConversationSerializer
 
 # Create your views here.
+
+
+class ConversationListView(APIView):
+    def get(self, request):
+        conversations = Conversation.objects.all()
+        serializer = ConversationSerializer(conversations, many=True)
+        return Response(serializer.data)
