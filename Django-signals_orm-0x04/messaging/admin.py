59f6e027e143d376ffd3212e0b3469e4cdd4e3ad
Emmanuel Rotich <71790505+mikemanuu@users.noreply.github.com>
2025-01-15 15:19:22 +0300

    implementing signals

---
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Message, Notification
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'timestamp')
+    search_fields = ('sender__username', 'receiver__username', 'content')
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'is_read', 'timestamp')
+    search_fields = ('user__username', 'message__content')
